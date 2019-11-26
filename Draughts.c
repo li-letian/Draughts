d8d50b17edbb88dd9f39f202d15db4a1fd0ff811
Letian <liletian@tongji.edu.cn>
2019-11-26 23:28:34 +0800

    完成输出
    优化了着法中的运算
    完成了输入输出
    增加了王棋的产生
    添加了注释

--- Draughts.c
+++ Draughts.c
@@ -4,6 +4,7 @@
 #define CHESS unsigned long long
 #define MAXLIST 1000010
 #define INFINITY 0x3f3f3f3f
+
 /*suppose the chessboard looks like
 						7
 						6
@@ -26,7 +27,6 @@ and the bit goes like
 */
 /**********PART 1 PREWORK*************/
 /*constant*/
-const int kMaxSize = 8;
 const CHESS kCutColumn[2] = { 0xfefefefefefefefe,0x7f7f7f7f7f7f7f7f };
 const CHESS kCutDoubleColumn[2] = { 0xfcfcfcfcfcfcfcfc,0x3f3f3f3f3f3f3f3f };
 const CHESS kKingRow = 0xff000000000000ff;
@@ -34,20 +34,27 @@ const CHESS kAllChessBoard = 0xffffffffffffffff;
 
 /*generate a specific direction bitboard move or jump*/
 
-/*suppose
+/*
+suppose
 the value of horizontal:0 means step right, 1 means step left
-the value of vertical:0 means step back, 1 means step forward*/
-/*chessboard[0]means white */
-/*chessboard[1]means black*/
-/*chessboard[2]means king*/
+the value of vertical:0 means step back, 1 means step forward
+chessboard[0]means white 
+chessboard[1]means black
+chessboard[2]means king
+*/
+/*return a next position */
 inline CHESS Move(const CHESS chessboard,const int horizontal,const int vertical)
 {
 	return (vertical) ?  (chessboard << ( horizontal? 9 : 7 ))&kAllChessBoard: (chessboard >> (horizontal? 7 : 9))&kAllChessBoard;
 }
+
+/*return the next position of the next position*/
 inline CHESS Jump(const CHESS chessboard,const  int horizontal,const int vertical)
 {
 	return (vertical) ? (chessboard << (horizontal ? 18 : 14))&kAllChessBoard : (chessboard >> (horizontal ? 14 : 18))&kAllChessBoard;
 }
+
+/*to count how many 1 in its  binary number*/
 inline int Count(CHESS chessboard)
 {
 	chessboard = ((chessboard >> 1) & 0x5555555555555555) + (chessboard & 0x5555555555555555);
@@ -79,23 +86,35 @@ inline void print(CHESS state)
 /*********PART 2 FIND WAYS TO GO**********/
 
 /*possible jump*/
+
+/*global variables*/
+
+/*the list to save the moving method and its index*/
+/*
+for each side and chessboard statement
+we generate a list including every possible single move or every possible jump
+and return its size
+*/
+CHESS list[MAXLIST][3];
+int index;
+
+/*the queue to bfs and its head and tail*/
 /*
 queue[i][0]saves chessboard of present statement of all my chess
 queue[i][1]saves chessboard of beaten enemy
 queue[i][2]saves chessboard of present extending location
-queue[i][3]saves numbers of the beaten enemy
 */
-
-/*global variables*/
-CHESS list[MAXLIST][3];
-int index;
 CHESS queue[150][3];
 int head = 1, tail = 0;
+
+/*reset a queue*/
 inline void QueueReset()
 { 
 	head = 1; 
 	tail = 0; 
 }
+
+/*push a node in the queue*/
 inline void QueuePush(const CHESS state,const CHESS bridge,const CHESS location)
 {
 	++tail;
@@ -103,6 +122,8 @@ inline void QueuePush(const CHESS state,const CHESS bridge,const CHESS location)
 	queue[tail][1] = bridge;
 	queue[tail][2] = location;
 }
+
+/*pop a node off the queue */
 inline void QueuePop(CHESS *state, CHESS *bridge, CHESS *location)
 {
 	(*state) = queue[head][0];
@@ -110,6 +131,8 @@ inline void QueuePop(CHESS *state, CHESS *bridge, CHESS *location)
 	(*location) = queue[head][2];
 	head++;
 }
+
+/*find possible jump in one direction*/
 inline void OneDirectionJump(const CHESS enemy,const CHESS state,const CHESS bridge,const CHESS location,const int horizontal,const int vertical)
 {
 	if(((Move(location,horizontal,vertical)&kCutColumn[horizontal^1])&(enemy^bridge))
@@ -118,6 +141,8 @@ inline void OneDirectionJump(const CHESS enemy,const CHESS state,const CHESS bri
 		QueuePush(state ^ (location | Jump(location, horizontal, vertical)), bridge | Move(location, horizontal, vertical), Jump(location, horizontal, vertical));
 	}
 }
+
+/*find all possible jump*/
 inline int  FindPossibleJump(const CHESS chessboard[],const int side)
 {
 	int start = index;
@@ -127,7 +152,7 @@ inline int  FindPossibleJump(const CHESS chessboard[],const int side)
 	while (state)
 	{
 		CHESS position = state & ((~state) + 1);
-		state = state ^ position;
+		state ^= position;
 		QueuePush(state, 0, position);
 	}
 	while (head <= tail)
@@ -152,25 +177,22 @@ inline int  FindPossibleJump(const CHESS chessboard[],const int side)
 }
 
 /*possible single move*/
-/*for each side and chessboard statement,it can generate a list including every possible single move and return its size*/
-/*suppose 
-0 means white,1 means black
-0 means step right, 1 means step left
-0 means step back, 1 means step forward*/
 inline void OneDirectionMove(const CHESS chessboard[], const int side,const int horizontal)
 {
 	CHESS move= ((Move(chessboard[side], horizontal, side ^ 1)&kCutColumn[horizontal^1])&(~chessboard[side ^ 1]))&kAllChessBoard;
 	while (move)
 	{
 		CHESS position = move & ((~move) + 1);
-		move = move ^ position;
-		position = position | Move(position, horizontal^1, side);
+		move ^= position;
+		position |= Move(position, horizontal^1, side);
 		index++;
 		list[index][side] = chessboard[side] ^ position;
 		list[index][side ^ 1] = chessboard[side ^ 1];
 		list[index][2] = (chessboard[2] & position) ? chessboard[2] ^ position : chessboard[2];
 	}
 }
+
+
 inline void KingExtralMove(const CHESS chessboard[], const int side, const int horizontal)
 {
 	CHESS move = ((Move(chessboard[side]&chessboard[2], horizontal, side)&kCutColumn[horizontal ^ 1])&(~chessboard[side ^ 1]))&kAllChessBoard;
@@ -178,15 +200,16 @@ inline void KingExtralMove(const CHESS chessboard[], const int side, const int h
 	{
 		CHESS position;
 		position = move ^ (move - 1);
-		position = position ^ (position >> 1);
-		move = move ^ position;
-		position = position | Move(position, horizontal ^ 1, side^1);
+		position ^= (position >> 1);
+		move ^= position;
+		position |= Move(position, horizontal ^ 1, side^1);
 		index++;
 		list[index][side] = chessboard[side] ^ position;
 		list[index][side ^ 1] = chessboard[side ^ 1];
 		list[index][2] =chessboard[2] ^ position;
 	}
 }
+
 inline int FindPossibleMove(const CHESS chessboard[],const int side)
 {
 	int start = index;
@@ -247,23 +270,86 @@ int AlphaBeta(const int depth, int alpha, int beta,const CHESS chessboard[],cons
 	return alpha;
 }
 
-void Print()
+inline int Transform(CHESS state)
 {
+	int temp = 0;
+	while (!(state&(1 << temp))) ++temp;
+	return temp;
+}
+
+int path[50];
+inline void Print(CHESS chessboard[],int side)
+{
+	int cnt = 2;
+	CHESS temp = chessboard[side] ^ output[side];
+	CHESS bridge = chessboard[side ^ 1] ^ output[side ^ 1];
+	int start = Transform(chessboard[side] & temp);
+	int end = Transform(output[side] & temp);
+	if (bridge)
+	{
+		printf("%d %d,%d", Count(bridge) + 1, start >> 3, start & 7);
+		CHESS position = chessboard[side] & temp;
+		while (bridge)
+		{
+			if (Move(position, 1, 1)&bridge)
+			{
+				bridge ^= Move(position, 1, 1);
+				position = Jump(position, 1, 1);
+				end = Transform(position);
+				printf(" %d,%d", end >> 3, end & 7);
+				fflush(stdout);
+				continue;
+			}
+			if(Move(position, 0, 1)&bridge)
+			{
+				bridge ^= Move(position, 0, 1);
+				position = Jump(position, 0, 1);
+				end = Transform(position);
+				printf(" %d,%d", end >> 3, end & 7);
+				fflush(stdout);
+				continue;
+			}
+			if (Move(position, 1, 0)&bridge)
+			{
+				bridge ^= Move(position, 1, 0);
+				position = Jump(position, 1, 0);
+				end = Transform(position);
+				printf(" %d,%d", end >> 3, end & 7);
+				fflush(stdout);
+				continue;
+			}
+			if (Move(position, 0, 0)&bridge)
+			{
+				bridge ^= Move(position, 0, 0);
+				position = Jump(position, 0, 0);
+				end = Transform(position);
+				printf(" %d,%d", end >> 3, end & 7);
+				fflush(stdout);
+				continue;
+			}
+		}
+	}
+	else
+	{
+		printf("%d %d,%d %d,%d", cnt,start>>3,start&7,end>>3,end&8);
+		fflush(stdout);
+	}
 
 
 }
 
 
 /**********PART 4 STDIN/STDOUT**************/
-void Search(CHESS *chessboard,const int side)
+void Search(CHESS chessboard[],const int side)
 {
 	AlphaBeta(6, -INFINITY, INFINITY, chessboard, side);
-	Print();
+	chessboard[2] |= chessboard[side] & kKingRow;
+	Print(chessboard, side);
 }
 
 inline CHESS Locate(int x, int y)
 {
-	return ((CHESS)1) << (x + 8 * y);
+	return ((CHESS)1) << ((y << 3) | x);
 }
 void Work(int side)
 {
@@ -291,7 +377,7 @@ void Work(int side)
 			{
 				scanf("%d,%d", &y[i], &x[i]);
 			}
-			chessboard[side] = chessboard[side] ^ (Locate(x[1], y[1]) | Locate(x[number], y[number]));
+			chessboard[side] ^= (Locate(x[1], y[1]) | Locate(x[number], y[number]));
 			chessboard[2] = Locate(x[1], y[1])&chessboard[2] ? chessboard[2] ^ (Locate(x[1], y[1]) | Locate(x[number], y[number])) : chessboard[2];
 			if ((x[1] - x[2])*(x[1] - x[2])*(y[1] - x[2])*(y[1] - x[2]) != 1)
 			{
@@ -299,8 +385,8 @@ void Work(int side)
 				{
 					int col = (y[i] + y[i - 1]) >> 1;
 					int row = (x[i] + x[i - 1]) >> 1;
-					chessboard[side ^ 1] = chessboard[side ^ 1] ^ Locate(row, col);
-					chessboard[2] = chessboard[2] ^ (chessboard[2] & Locate(row, col));
+					chessboard[side ^ 1] ^= Locate(row, col);
+					chessboard[2] ^= (chessboard[2] & Locate(row, col));
 				}
 			}
 			continue;
